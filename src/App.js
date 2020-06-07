32d56e4c34aa6a8a41ca9651723452b2647e716b
minxd95 <newobj95@gmail.com>
2020-06-07 18:52:04 +0900

    업로드

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [color, setColor] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Pallete color={color} />
+      <ButtonContainer>
+        <CyanButton onClick={() => setColor("cyan")} />
+        <BrownButton onClick={() => setColor("brown")} />
+        <PinkButton onClick={() => setColor("pink")} />
+      </ButtonContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Pallete = styled.div`
+  width: 700px;
+  height: 700px;
+  border: 1px solid black;
+  background: ${(props) => props.color};
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 50px;
+  width: 700px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  margin: 0 5px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`;
+
+const CyanButton = styled(Button)`
+  background: cyan;
+`;
+
+const BrownButton = styled(Button)`
+  background: brown;
+`;
+
+const PinkButton = styled(Button)`
+  background: pink;
+`;
 
 export default App;
